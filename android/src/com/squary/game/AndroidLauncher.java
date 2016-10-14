3e2fc360f7da0b42d020db08aaf75c4202d6237a
Dimitar Borisov <dimitar.a.borisov@gmail.com>
2016-10-14 20:26:24 +0100

    initial commit

---
+++ android/src/com/squary/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.squary.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.squary.game.GameSquary;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameSquary(), config);
+	}
+}
---
+++ desktop/src/com/squary/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.squary.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.squary.game.GameSquary;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameSquary(), config);
+	}
+}
