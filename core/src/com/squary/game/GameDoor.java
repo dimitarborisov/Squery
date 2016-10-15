a758b988f031e55d9373a89c494e45030cd5b2ac
Dimitar Borisov <dimitar.a.borisov@gmail.com>
2016-10-15 14:12:40 +0100

    added doors and background

--- core/src/com/squary/game/GameDoor.java
+++ core/src/com/squary/game/GameDoor.java
@@ -1,22 +1,73 @@
 package com.squary.game;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 public class GameDoor extends GameEntities {
 
+	boolean locked;
+	Sprite doorSprite;
+	Sprite lockSprite;
+	float sizeX,sizeY;
+	
 	public GameDoor(GameSquary game) {
 		super(game);
-		// TODO Auto-generated constructor stub
+		locked = true;
+		
+		doorSprite = new Sprite(game.getTextureManager().getTexture("locked_door"));
 	}
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
+		game.getSpriteBatch().begin();
+			
+		if(locked){
+			doorSprite.draw(game.getSpriteBatch());
+		}
+		
+		game.getSpriteBatch().end();
 
 	}
 
 	@Override
 	public void update(float dt) {
-		// TODO Auto-generated method stub
+		doorSprite.setX(x);
+		doorSprite.setY(y);
+		doorSprite.setSize(sizeX, sizeY);
 
 	}
 
+	public void setSizeX(float size){
+		sizeX = size;
+	}
+	
+	public void setSizeY(float size){
+		sizeY = size;
+	}
+	
+	public void setPosY(float y){
+		this.y = y;
+	}
+	
+	public void setPosX(float x){
+		this.x = x;
+	}
+	
+	public void setPos(float x, float y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void setSize(float width, float height){
+		this.sizeX = width;
+		this.sizeY = height;
+	}
+	
+	public boolean getLocked(){
+		return locked;
+	}
+	
+	public void setLocked(boolean locked){
+		this.locked = locked;
+	}
 }
