9b0c111eddc9d8de8ede30de7b3bcf16982ac442
Dimitar Borisov <dimitar.a.borisov@gmail.com>
2016-10-15 18:11:42 +0100

    added splash screen animation

--- core/src/com/squary/game/GameSquary.java
+++ core/src/com/squary/game/GameSquary.java
@@ -42,6 +42,10 @@ public class GameSquary implements ApplicationListener {
 		manager.loadTexture("locked_door", "lockDoor.png");
 		manager.loadTexture("locker", "lock.png");
 		manager.loadTexture("wall", "walls.png");
+		manager.loadTexture("logo2-background", "logo2-background.png");
+		manager.loadTexture("logo2-e1", "logo2-e1.png");
+		manager.loadTexture("logo2-e2", "logo2-e2.png");
+		manager.loadTexture("logo2-e3", "logo2-e3.png");
 		
 		gsm = new GameStateManager(this);
 	}
