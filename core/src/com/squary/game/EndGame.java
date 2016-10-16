c698c5407e8aca88580020baf4fe3e4795a7e28a
Dimitar Borisov <dimitar.a.borisov@gmail.com>
2016-10-16 09:22:09 +0100

    added final screen

--- core/src/com/squary/game/EndGame.java
+++ core/src/com/squary/game/EndGame.java
@@ -2,7 +2,9 @@ package com.squary.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
@@ -14,34 +16,33 @@ public class EndGame extends GameState {
 	FreeTypeFontParameter parameter;
 	BitmapFont font12;
 	InputProcessor inputProcessor;
-	
+	BitmapFont font;
+	GlyphLayout layout;
+
 	Sprite background;
-	
+
 	protected EndGame(GameStateManager gsm) {
 		super(gsm);
-
-		generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/myfont.ttf"));
-		parameter = new FreeTypeFontParameter();
-		parameter.size = 12;
-		
-		font12 = generator.generateFont(parameter); // font size 12 pixels
-		generator.dispose(); // don't forget to dispose to avoid memory leaks!
-		
 		background = new Sprite(am.getTexture("background"));
+
+		font = new BitmapFont(Gdx.files.internal("font/Zrinc.fnt"), Gdx.files.internal("font/Zrinc.png"), false);
+		layout = new GlyphLayout(); //dont do this every frame! Store it as member
+		layout.setText(font, "Final score: " + SCORE);
+		
 		
 		inputProcessor = new InputProcessor() {
 
 			@Override
 			public boolean keyDown(int keycode) {
-				// TODO Auto-generated method stub
+				Play.STATE = -1;
+				EndGame.this.getStateManager().setState(new FadeOutIn(EndGame.this.getStateManager(), EndGame.this,
+						new Menu(EndGame.this.getStateManager()), false, false));
 				return false;
 			}
 
 			@Override
 			public boolean keyUp(int keycode) {
-				Play.STATE = -1;
-				EndGame.this.getStateManager().setState(new FadeOutIn(EndGame.this.getStateManager(), EndGame.this, new Menu(EndGame.this.getStateManager()), false, false));
-				
+
 				return false;
 			}
 
@@ -53,14 +54,14 @@ public class EndGame extends GameState {
 
 			@Override
 			public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-				// TODO Auto-generated method stub
+				EndGame.this.getStateManager().setState(new FadeOutIn(EndGame.this.getStateManager(), EndGame.this,
+						new Menu(EndGame.this.getStateManager()), false, false));
 				return false;
 			}
 
 			@Override
 			public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-				EndGame.this.getStateManager().setState(new FadeOutIn(EndGame.this.getStateManager(), EndGame.this, new Menu(EndGame.this.getStateManager()), false, false));
-				return true;
+				return false;
 			}
 
 			@Override
@@ -93,7 +94,13 @@ public class EndGame extends GameState {
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
+		sb.begin();
+		background.draw(sb);
+
+		// sprite.draw(batch);
+		font.draw(sb, layout, (GameSquary.VWIDTH / 2) - layout.width / 2, (GameSquary.VHEIGHT / 2) + layout.height / 2);
+
+		sb.end();
 
 	}
 
